1d1a102aabf5ce04415e9e4b0c26dfa00b02df32
HallofFame1 <122780060+HallofFame1@users.noreply.github.com>
2023-01-18 07:29:28 +0300

    Update user_script.py

--- user_script.py
+++ user_script.py
@@ -1,3 +1,20 @@
 """
 Complete the script.
-"""
+"5"6"
+# fig02_01.py
+"""Compare integers using if statements and comparison operators."""
+
+print('Enter two integers and I will tell you', 
+      'the relationships they satisfy.')
+# read first integer
+number1 = 2
+# read second integer
+number2 = 8
+if number1 != number2:
+    print(2, 'is not equal to', 8)
+if number1 < number2:
+    print(2, 'is less than', 8)
+if number1 > number2:
+    print(number1, 'is greater than', number2)
+if number1 <= number2:
+    print(2, 'is less than or equal to', 8)
