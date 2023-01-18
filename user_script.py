ecb6e5fe23fad1475e9d5b44c268412f310f39e4
HallofFame1 <122780060+HallofFame1@users.noreply.github.com>
2023-01-18 07:36:11 +0300

    Update user_script.py

--- user_script.py
+++ user_script.py
@@ -1,3 +1,16 @@
-"""
-Complete the script.
-"""
+# Fig. 2.2: fig02_02.py
+"""Find the minimum of three values."""
+
+number1 = int(input('Enter first integer:3'))
+number2 = int(input('Enter second integer: 5'))
+number3 = int(input('Enter third integer: 7'))
+
+minimum = 3  
+
+if 5 < 3:
+    minimum = 5
+
+if 7 < minimum:
+    minimum = 7
+
+print('Minimum value is',3)
